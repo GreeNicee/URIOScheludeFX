a39a8c60988e9e40b87ef6e5671620f9904cee65
GreeN <ivan550zero@mail.ru>
2016-10-07 21:47:55 +0300

    "First commit"

---
+++ src/ru/greenstudio/urioschedulefx/MainApp.java
@@ -0,0 +1,51 @@
+package ru.greenstudio.urioschedulefx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    public MainApp() {
+    }
+
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("УРИО - составление расписания");
+//            this.primaryStage.getIcons().add(new Image("file:resources/images/adress_book.png"));
+
+        initRootLayout();
+    }
+
+    public void initRootLayout() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/ru/greenstudio/urioschedulefx/view/RootLayoutController.java
@@ -0,0 +1,14 @@
+package ru.greenstudio.urioschedulefx.view;
+
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by ivan5 on 07.10.2016.
+ */
+public class RootLayoutController {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+
+}
