892ed204d4567f98e0fbdd636576f8a596c2b19e
GreeN <ivan550zero@mail.ru>
2016-10-08 16:16:50 +0300

    add tab LessonsAndCabs (without funcs)

--- src/ru/greenstudio/urioschedulefx/MainApp.java
+++ src/ru/greenstudio/urioschedulefx/MainApp.java
@@ -1,8 +1,14 @@
 package ru.greenstudio.urioschedulefx;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -22,6 +28,8 @@ public class MainApp extends Application {
 //            this.primaryStage.getIcons().add(new Image("file:resources/images/adress_book.png"));
 
         initRootLayout();
+
+        showLessonsAndCabs();
     }
 
     public void initRootLayout() {
@@ -36,6 +44,40 @@ public class MainApp extends Application {
             primaryStage.setScene(scene);
 
             primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showLessonsAndCabs() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/LessonsAndCabsLayout.fxml"));
+            SplitPane lessAndCabs = (SplitPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            for (int i = 0; i < rootLayout.getChildren().size(); i++) {
+                ObservableList<Node> rootChilds = rootLayout.getChildren();
+                if (rootChilds.get(i) instanceof TabPane) {
+                    ObservableList<Tab> rootTabs = ((TabPane) rootChilds.get(i)).getTabs();
+                    for (int j = 0; j < rootTabs.size(); j++) {
+                        String tabId = "";
+                        Tab tab = (Tab) rootTabs.get(j);
+                        if (rootTabs.get(j).getId() != null)
+                            tabId = rootTabs.get(j).getId();
+                        if (tabId.equals("tabLessonsAndCabs")) {
+                            AnchorPane anchorPane = (AnchorPane) tab.getContent();
+                            anchorPane.getChildren().add(lessAndCabs);
+
+                            anchorPane.setTopAnchor(lessAndCabs, 0.0);
+                            anchorPane.setBottomAnchor(lessAndCabs, 0.0);
+                            anchorPane.setLeftAnchor(lessAndCabs, 0.0);
+                            anchorPane.setRightAnchor(lessAndCabs, 0.0);
+                        }
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/ru/greenstudio/urioschedulefx/view/RootLayoutController.java
+++ src/ru/greenstudio/urioschedulefx/view/RootLayoutController.java
@@ -1,14 +1,53 @@
 package ru.greenstudio.urioschedulefx.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ru.greenstudio.urioschedulefx.MainApp;
 
-/**
- * Created by ivan5 on 07.10.2016.
- */
 public class RootLayoutController {
-    private Stage primaryStage;
-    private BorderPane rootLayout;
+    private MainApp mainApp;
 
+    @FXML
+    private TabPane tabPane ;
+    @FXML
+    private Tab tabSchedule;
+    @FXML
+    private Tab tabTeachers;
+    @FXML
+    private Tab tabGroups;
+    @FXML
+    private Tab tabLessonsAndCabs;
 
+    public Tab getTabLessonsAndCabs() {
+        return tabLessonsAndCabs;
+    }
+
+    @FXML
+
+    private Tab tabTimeSchedule;
+
+    @FXML
+    private void initialize() {
+        // Initialize the person table with the two columns.
+//        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+//        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Add observable list data to the table
+//        personTable.setItems(mainApp.getPersonData());
+    }
+
+    public RootLayoutController() {
+    }
 }
